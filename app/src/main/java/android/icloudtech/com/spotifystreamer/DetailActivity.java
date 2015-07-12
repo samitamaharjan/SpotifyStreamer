3b90a1c614c4c346a3d2db703c96b69fd07d9716
Samita Maharjan <samitamaharjan@gmail.com>
2015-07-12 11:06:50 -0400

    Displaying settings screen

--- app/src/main/java/android/icloudtech/com/spotifystreamer/DetailActivity.java
+++ app/src/main/java/android/icloudtech/com/spotifystreamer/DetailActivity.java
@@ -56,6 +56,7 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
